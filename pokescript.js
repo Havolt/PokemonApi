427bb41e5c9230c274d327c3599179ece1b2c94f
Mark Fitzpatrick <markfitz815@gmail.com>
2018-05-03 20:56:33 +0100

    application can now retrieve sprite of pokemon and display it on screen

--- pokescript.js
+++ pokescript.js
@@ -1,9 +1,35 @@
-console.log('poke')
 
-let require = new XMLHttpRequest();
+let request = new XMLHttpRequest();
+let requestURL;
 
 document.querySelector('.pokeSubmit').onclick = function(){
-    console.log(document.querySelector('.pokeInput').value);
+    pokeGet(document.querySelector('.pokeInput').value);
     document.querySelector('.pokeInput').value = '';
 }
 
+
+function pokeGet(val){
+    requestURL = 'http://pokeapi.salestock.net/api/v2/pokemon/'+val;
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    pokeWait();
+}
+
+function pokeWait(){
+    if(request.response){
+        pokeInit();
+    }else{
+        setTimeout(function(){
+            pokeWait()
+        }, 50)
+    }
+}
+
+function pokeInit(){
+    console.log(request.response)
+
+    let sprite = document.createElement('img');
+    sprite.src = request.response.sprites.front_default;
+    document.body.appendChild(sprite);
+}
